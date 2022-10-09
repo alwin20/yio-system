1229cee5be073c8d65d9af5830e93cde2d3d6a38
yio <alwin_yang@foxmail.com>
2022-10-09 11:46:03 +0800

    第一次提交

---
+++ yio-api/src/main/java/com/yio/YioApiApplication.java
@@ -0,0 +1,13 @@
+package com.yio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YioApiApplication.class, args);
+    }
+
+}
